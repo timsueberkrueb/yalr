5baafae1c79254cb03e5110cca6ad6ba15559df6
Tim Süberkrüb <dev@timsueberkrueb.io>
2018-12-23 20:13:49 +0100

    Add initial module docs

--- yalr/src/lib.rs
+++ yalr/src/lib.rs
@@ -1,3 +1,20 @@
+//! # YALR
+//!
+//! YALR (Yet Another LR parser generator) is a Rust library for generating `LALR(1)` parsers.
+//!
+//! YALR takes an `impl` block containing regular Rust functions augmented with grammar rule
+//! attributes and generates a parser implementation.
+//!
+//! All of this is done using [Rust's procedural macros](https://doc.rust-lang.org/reference/procedural-macros.html).
+//!
+//! ## Usage
+//!
+//! Start by checking out the [`lalr` attribute](../yalr_proc_macro/attr.lalr.html), the starting
+//! point for any YALR parser.
+//!
+//! Also take a look at the `examples` folder.
+//!
+
 mod error;
 pub mod extra;
 mod traits;
