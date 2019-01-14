05b8b5571de82528b567f6ded0f4be2917ca33a9
Tim Süberkrüb <dev@timsueberkrueb.io>
2019-01-14 21:20:31 +0100

    Update JSON example for YALR changes

--- examples/json/src/lib.rs
+++ examples/json/src/lib.rs
@@ -1,10 +1,12 @@
+// FIXME: Upstream bug (https://github.com/maciejhirsz/logos/issues/66)
+#![allow(macro_expanded_macro_exports_accessed_by_absolute_paths)]
+
 use std::fmt;
 use std::collections::HashMap;
 
 use logos::Logos;
 use yalr::extra::LogosSupport;
 use yalr::*;
-
 
 type JSONString = String;
 type JSONNumber = f64;
@@ -44,7 +46,7 @@ impl fmt::Display for Nonterminal {
 }
 
 #[derive(Logos, Ord, PartialOrd, Debug, Clone, Eq, PartialEq, Hash)]
-enum Terminal {
+pub enum Terminal {
     // {m,n} notation is currently not supported
     #[regex = r#""([^"\\\t\n]|\\"|\\\\|\\/|\\b|\\f|\\n|\\r|\\t|\\u[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9])*""#]
     String,
@@ -82,43 +84,39 @@ impl fmt::Display for Terminal {
 
 pub struct Parser;
 
-#[lalr(Terminal, Nonterminal)]
-#[start_symbol(Start)]
-#[end_terminal(End)]
-#[input(str)]
-#[output(JSONValue)]
+impl<'input> YALR<'input> for Parser {
+    type Terminal = Terminal;
+    type Input = &'input str;
+    type Output = JSONValue;
+}
+
+#[lalr(start = "Value")]
+#[terminal_type(Terminal)]
 impl Parser {
-    
     // Utility function
-    pub fn parse_str(s: &str) -> Result<JSONValue, Box<dyn std::error::Error>> {
+    pub fn parse_str(s: &str) -> Result<JSONValue, ParseError<Terminal>> {
         let lexer = Terminal::lexer(s);
         Parser::parse_logos(lexer)
     }
- 
- 
-    #[rule(Start -> Value end)]
-    fn start(value: JSONValue, _end: ()) -> JSONValue {
-        value
-    }
 
-    #[rule(String -> string)]
+    #[rule(StringValue -> String)]
     fn string(input: &str) -> JSONString {
         // This is guaranteed to cut at valid utf-8 borders
-        // becasue first and last character are always quotes
+        // because first and last character are always quotes
         input[1..(input.len() - 1)].to_owned()
     }
 
-    #[rule(Number -> number)]
+    #[rule(NumberValue -> Number)]
     fn number(input: &str) -> JSONNumber {
         input.parse().unwrap()
     }
     
-    #[rule(Value -> Number)]
+    #[rule(Value -> NumberValue)]
     fn value_number(num: JSONNumber) -> JSONValue {
         JSONValue::Number(num)
     }
     
-    #[rule(Value -> String)]
+    #[rule(Value -> StringValue)]
     fn value_string(string: JSONString) -> JSONValue {
         JSONValue::String(string)
     }
@@ -133,84 +131,82 @@ impl Parser {
         JSONValue::Object(obj)
     }
     
-    #[rule(Value -> null)]
+    #[rule(Value -> Null)]
     fn value_null(_null: &str) -> JSONValue {
         JSONValue::Null
     }
 
-    #[rule(Value -> true)]
+    #[rule(Value -> True)]
     fn value_true(_true: &str) -> JSONValue {
         JSONValue::True
     }
     
-    #[rule(Value -> false)]
+    #[rule(Value -> False)]
     fn value_false(_false: &str) -> JSONValue {
         JSONValue::False
     }
 
-    
-    #[rule(Object -> leftbrace rightbrace)]
+    #[rule(Object -> Leftbrace Rightbrace)]
     fn object_empty(_lbrace: &str, _rbrace: &str) -> JSONObject {
         HashMap::new()
     }
 
-    #[rule(Object -> leftbrace String colon Value rightbrace)]
+    #[rule(Object -> Leftbrace StringValue Colon Value Rightbrace)]
     fn object_one(_lbrace: &str, key: JSONString, _color: &str, value: JSONValue, _rbrace: &str) -> JSONObject {
         let mut res = HashMap::new();
         res.insert(key, value);
         res
     }
 
-    #[rule(Object -> leftbrace KVPairMap String colon Value rightbrace)]
+    #[rule(Object -> Leftbrace KVPairMap StringValue Colon Value Rightbrace)]
     fn object_many(_lbrace: &str, mut kpm: KVPairMap, key: JSONString, _color: &str, value: JSONValue, _rbrace: &str) -> JSONObject {
         kpm.insert(key, value);
         kpm
     }
 
-    #[rule(KVPairMap -> KVPairMap String colon Value comma)]
+    #[rule(KVPairMap -> KVPairMap StringValue Colon Value Comma)]
     fn kvpairmap_extend(mut kpm: KVPairMap, key: JSONString, _colon: &str, value: JSONValue, _comma: &str) -> KVPairMap {
         kpm.insert(key, value);
         kpm
     }
     
-    #[rule(KVPairMap -> String colon Value comma)]
+    #[rule(KVPairMap -> StringValue Colon Value Comma)]
     fn kvpairlist_begin(key: JSONString, _colon: &str, value: JSONValue, _comma: &str) -> KVPairMap {
         let mut kpm = HashMap::new();
         kpm.insert(key, value);
         kpm
     }
     
-    #[rule(Array -> leftbracket rightbracket)]
+    #[rule(Array -> Leftbracket Rightbracket)]
     fn array_empty(_lbracket: &str, _rbracket: &str) -> JSONArray {
         Vec::new()
     }
 
-    #[rule(Array -> leftbracket Value rightbracket)]
+    #[rule(Array -> Leftbracket Value Rightbracket)]
     fn array_one(_lbracket: &str, val: JSONValue, _rbracket: &str) -> JSONArray {
         let mut res = Vec::new();
         res.push(val);
         res
     }
     
-    #[rule(Array -> leftbracket ArrayInner Value rightbracket)]
+    #[rule(Array -> Leftbracket ArrayInner Value Rightbracket)]
     fn array_many(_lbracket: &str, mut inner: ArrayInner, val: JSONValue, _rbracket: &str) -> JSONArray {
         inner.push(val);
         inner
     }
 
-    #[rule(ArrayInner -> ArrayInner Value comma)]
+    #[rule(ArrayInner -> ArrayInner Value Comma)]
     fn array_inner_extend(mut inner: ArrayInner, val: JSONValue, _comma: &str) -> ArrayInner {
         inner.push(val);
         inner
     }
 
-    #[rule(ArrayInner -> Value comma)]
+    #[rule(ArrayInner -> Value Comma)]
     fn array_inner_begin(val: JSONValue, _comma: &str) -> ArrayInner {
         let mut res = Vec::new();
         res.push(val);
         res
     }
-
 }
 
 mod test {
